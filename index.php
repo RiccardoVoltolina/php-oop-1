233064b5dbc665c9c5fa2f12e4366be84d4130df
Riccardo <riky.volto@live.com>
2023-10-19 17:33:45 +0200

    add film image

--- index.php
+++ index.php
@@ -12,50 +12,44 @@ organizzando il layout dividendo la struttura ed i contenuti in file e parziali 
 
 
 <?php
-class Movie {
+class Movie
+{
     public $name;
     public $author;
     public $valutationRateo;
     public $filmGenre;
     public $valutation;
+    public $movieImage;
 
-function __construct($name, $author, $filmGenre, $valutation) {
-    $this->name = $name;
-    $this->author = $author;
-    $this->filmGenre = $filmGenre;
-    $this->valutation = $valutation;
-
-}
-public function getFilmRaputation() {
-    if ($this->valutation < 3) {
-        $this->valutationRateo = 'Questo film non è piaciuto agli utenti';
-    } else {
-        $this->valutationRateo = 'Questo film è piaciuto agli utenti';
+    function __construct($name, $author, $filmGenre, $valutation, $movieImage)
+    {
+        $this->name = $name;
+        $this->author = $author;
+        $this->filmGenre = $filmGenre;
+        $this->valutation = $valutation;
+        $this->movieImage = $movieImage;
     }
-
+    public function getFilmRaputation()
+    {
+        if ($this->valutation < 3) {
+            $this->valutationRateo = 'Questo film non è piaciuto agli utenti';
+        } else {
+            $this->valutationRateo = 'Questo film è piaciuto agli utenti';
+        }
+    }
 }
 
-
-
-
-}
-
-$avatar_film = new Movie('Avatar', 'James Cameron', 'Action', 3);
-$up_film = new Movie('Up', 'Pete Docter', 'Cartoon', 4);
-var_dump($avatar_film);
+$avatar_film = new Movie('Avatar', 'James Cameron', 'Action', 2, 'https://picsum.photos/200/300');
+$up_film = new Movie('Up', 'Pete Docter', 'Cartoon', 4, 'https://picsum.photos/200/300');
 
 $up_film->getFilmRaputation();
 $valutation_up_film = $up_film->getFilmRaputation();
-var_dump($up_film);
 
 $up_film->getFilmRaputation();
 $valutation_avatar_film = $avatar_film->getFilmRaputation();
-var_dump($avatar_film);
 
-$movieArray = array($avatar_film,$up_film);
+$movieArray = array($avatar_film, $up_film);
 
-
-var_dump($movieArray);
 
 
 
@@ -68,17 +62,32 @@ var_dump($movieArray);
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>php oop</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+    <style>
+        img {
+            height: 300px;
+            width: 200px;
+        }
+    </style>
 </head>
+
 <body>
-    <h5><?= $movieArray[1]->name; ?></h5>
-    <?php foreach($movieArray as $movie):  ?>
-        <div>
+    <!-- <h5><?= $movieArray[1]->name; ?></h5>  print without foreach-->
+    <?php foreach ($movieArray as $movie) :  ?>
+        <div class="card">
             <h1><?php echo $movie->name ?></h1>
+            <img src="<?= $movie->movieImage ?>">
+            <div><?php echo $movie->author ?></div>
+            <div><?php echo $movie->filmGenre ?></div>
+            <div><?php echo $movie->valutation ?></div>
         </div>
+        <h5><?php echo $movie->valutationRateo ?></h5>
     <?php endforeach; ?>
 </body>
+
 </html>
